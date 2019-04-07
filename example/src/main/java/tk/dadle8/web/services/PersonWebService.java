a47d43cdbd052a71d92e83e512068e5f4807f11e
dadle8 <dadly8@gmail.com>
2019-04-07 20:32:07 +0300

    Create example

---
+++ example/src/main/java/tk/dadle8/web/services/PersonWebService.java
@@ -0,0 +1,14 @@
+package tk.dadle8.web.services;
+
+import java.util.List;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+@WebService(serviceName = "PersonService")
+public class PersonWebService {
+    @WebMethod(operationName = "getPersons")
+    public List<Person> getPersons() {
+        PostgreSQLDAO dao = new PostgreSQLDAO();
+        List<Person> persons = dao.getPersons();
+        return persons;
+    }
+}
