35cfeb502190890fadbd6a8641e5ca0c79b56cae
dadle8 <dadly8@gmail.com>
2019-04-09 12:20:27 +0300

    Generate client fo SOAP

---
+++ task1/src/main/java/tk/dadle8/web/services/client/generated/ObjectFactory.java
@@ -0,0 +1,79 @@
+
+package tk.dadle8.web.services.client.generated;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the tk.dadle8.web.services.client.generated package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _GetCountriesResponse_QNAME = new QName("http://services.web.dadle8.tk/", "getCountriesResponse");
+    private final static QName _GetCountries_QNAME = new QName("http://services.web.dadle8.tk/", "getCountries");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: tk.dadle8.web.services.client.generated
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetCountries }
+     * 
+     */
+    public GetCountries createGetCountries() {
+        return new GetCountries();
+    }
+
+    /**
+     * Create an instance of {@link GetCountriesResponse }
+     * 
+     */
+    public GetCountriesResponse createGetCountriesResponse() {
+        return new GetCountriesResponse();
+    }
+
+    /**
+     * Create an instance of {@link Country }
+     * 
+     */
+    public Country createCountry() {
+        return new Country();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link GetCountriesResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://services.web.dadle8.tk/", name = "getCountriesResponse")
+    public JAXBElement<GetCountriesResponse> createGetCountriesResponse(GetCountriesResponse value) {
+        return new JAXBElement<GetCountriesResponse>(_GetCountriesResponse_QNAME, GetCountriesResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link GetCountries }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://services.web.dadle8.tk/", name = "getCountries")
+    public JAXBElement<GetCountries> createGetCountries(GetCountries value) {
+        return new JAXBElement<GetCountries>(_GetCountries_QNAME, GetCountries.class, null, value);
+    }
+
+}
